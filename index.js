06d55580c275fe97b9d1415ae9341dbc79221a4a
Paul Cavallo <greendrone246@gmail.com>
2020-11-25 17:48:08 -0800

    v2

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const startServer = async () => {
   });
 
   const server = new ApolloServer({ typeDefs, resolvers });
-  server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`)
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
   });
 };
 
